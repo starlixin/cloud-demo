8af55463b1e21748ff2bdffb366f5397067a7b4e
Xin.Li <xin.li@androidmov.com>
2021-06-23 18:02:25 +0800

    Initial commit

---
+++ core/src/test/java/com/lx/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lx.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
